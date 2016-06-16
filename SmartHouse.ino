1e9b23e003e8517ec85f521cffa1cd2e89b74bde
Wendler Zacariotto <wendler2013@hotmail.com>
2016-06-16 14:23:41 -0300

    Add files via upload

---
+++ SmartHouse.ino
@@ -0,0 +1,18 @@
+void setup()  
+{  
+  Serial.begin(9600);  
+  //Abaixo, as portas quer serão ativadas pelo celular, via Bluetooth  
+  pinMode(5,OUTPUT);  
+  pinMode(6,OUTPUT);  
+  pinMode(7,OUTPUT);  
+}  
+    
+void loop()  
+{  
+  if(Serial.available()>=2)  
+  {  
+    int key=Serial.read();  
+    int val=Serial.read();  
+    digitalWrite(key,val);  
+  }  
+} 
